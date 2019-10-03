fa929ff266562bade9a461f55600382a771dac9f
Jean Prado <jeanfprado@gmail.com>
2019-10-03 14:47:00 -0300

    alteração teste

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM golang:1.7.4-alpine3.5
-
-WORKDIR /src
-
-COPY src/golang-ci/main.go ./
-COPY src/golang-ci/main_test.go ./
-
-EXPOSE 8080
-CMD ["go", "run", "main.go"]
--- main_test.go
+++ main_test.go
@@ -1,10 +1,12 @@
 package main
+
 import "testing"
 
 func TestSum(t *testing.T) {
     got := sum(5,5)
 
     if got != 10 {
-        t.Errorf("Erro na soma", got)
+       return  t.Errorf("Erro na soma", got)
     }
+    return nil
 }
